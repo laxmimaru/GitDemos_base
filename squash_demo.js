594deac5f60c51bd75b2f6393b37ff3e60469f2c
Laxmi Kanth <laxmimaru28@gmail.com>
2024-01-25 21:44:54 +0530

    commit** 1

--- squash_demo.js
+++ squash_demo.js
@@ -1,1 +1,15 @@
 console.log('main commit');
+
+console.log(' commit---1')
+
+console.log(' commit---2')
+
+console.log(' commit---3')
+
+console.log(' commit---3')
+
+console.log(' commit---4')
+
+console.log(' commit---5')
+
+console.log(' commit---61')
